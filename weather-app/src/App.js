c87be4913bf2e632a458f1cfaddb161be489a9bd
Yovento <jovento@gmail.com>
2019-12-17 14:50:08 -0500

    Updates

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import WeatherLocation from './components/WeatherLocation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       Weather App (aplicacion del clima)
+      <WeatherLocation />
     </div>
   );
 }
